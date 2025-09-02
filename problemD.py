a55ec99acf403232c7ab26ebdc302baa0d470ead
Aaron Nguyen <minh7898888@gmail.com>
2025-09-01 20:53:40 -0700

    commit

--- problemD.py
+++ problemD.py
@@ -1,2 +1,9 @@
 import random
 import string
+
+def create_crossword(words):
+    grid = [[None for _ in range(10)] for _ in range(10)] # initialize a blank 10x10 grid 
+    
+    directions = [
+
+    ]
