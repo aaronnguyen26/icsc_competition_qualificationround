c48aca18c989317f988e10974dbbee7eb270054e
Aaron Nguyen <minh7898888@gmail.com>
2025-09-01 21:03:46 -0700

    commit

--- problemD.py
+++ problemD.py
@@ -5,5 +5,12 @@ def create_crossword(words):
     grid = [[None for _ in range(10)] for _ in range(10)] # initialize a blank 10x10 grid with none values 
     
     directions = [
-
+        (0,1), # horizontal from left to right
+        (1,0), # vertical from top to bottom 
+        (1,1), # diagional from top left to bottom right 
+        (0,-1),
+        (-1,0),
+        (-1,-1),
+        (1,-1),
+        (-1,1)
     ]
